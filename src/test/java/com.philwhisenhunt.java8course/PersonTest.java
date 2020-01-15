db101214c3f0d6304bf1df37d9b6a2a1e775fa4e
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-15 18:30:36 -0500

    Fixed typos but still getting an error about main

--- src/test/java/com.philwhisenhunt.java8course/PersonTest.java
+++ src/test/java/com.philwhisenhunt.java8course/PersonTest.java
@@ -3,9 +3,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PersonTest {
+    
     @Test
     public void shouldReturnHelloWorld() {
         Person phil = new Person();
-        assertEquals("Hello World", marcus.helloWorld() ); 
+        assertEquals("Hello World", phil.helloWorld() ); 
     }
 }
