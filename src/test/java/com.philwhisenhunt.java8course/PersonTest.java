444a7eafd79ed44d7c090feb401f3c7ba564b872
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-14 21:04:05 -0500

    Now importing test so that things will work soon

--- src/test/java/com.philwhisenhunt.java8course/PersonTest.java
+++ src/test/java/com.philwhisenhunt.java8course/PersonTest.java
@@ -1,8 +1,11 @@
 package com.philwhisenhunt.java8course;
-import org.junit.Test
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class PersonTest {
     @Test
     public void shouldReturnHelloWorld() {
         Person phil = new Person();
+        assertEquals("Hello World", marcus.helloWorld() ) 
     }
 }
